9993547cf4f9ad44ddfa8bc89e7a317863b9a320
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2023-08-24 01:39:03 -0300

    Added error handling

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,8 @@ import { useMaskedInput } from '../assets/hooks/useMaskedInput'
 import NotificationsIllustration from '../assets/vectors/illustration-push-notifications.svg'
 import { EInputMasks } from '../models/constants/EInputMasks'
 import { cn } from '../utils/cn'
-import { Link } from 'expo-router'
+import { router } from 'expo-router'
+import { useEffect, useState } from 'react'
 import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import colors from 'tailwindcss/colors'
 
@@ -10,44 +11,62 @@ export default function SignIn() {
   const [phoneNumber, handlePhoneNumberChange, isPhoneNumberValid] = useMaskedInput(
     EInputMasks.PHONE_NUMBER,
   )
+  const [showError, setShowError] = useState(false)
+
+  const handleReceiveOTP = () => {
+    if (!isPhoneNumberValid) return setShowError(true)
+    router.push('/otpConfirmation')
+  }
+
+  useEffect(() => {
+    setShowError(false)
+  }, [phoneNumber])
 
   return (
-    <SafeAreaView className="flex flex-1 bg-red-500">
-      <View className="flex-1 bg-white px-6 py-2">
+    <SafeAreaView className="flex flex-1 bg-white">
+      <View className="flex-1 px-6 py-2">
         <View className="my-8 w-fit items-center">
-          <NotificationsIllustration height={112} />
+          <NotificationsIllustration height={112} pointerEvents="none" />
         </View>
         <Text className="text-xl font-medium">Qual o número do seu celular?</Text>
         <Text className="text-base font-normal text-gray-500">Você receberá um código por SMS</Text>
-        <View
-          data-test={isPhoneNumberValid}
-          className="my-6 h-12 flex-row justify-center rounded border border-gray-200 px-3"
-        >
-          <Text className="mr-2 self-center text-base text-gray-400" style={{ lineHeight: 20 }}>
-            +55
-          </Text>
-          <View style={{ flex: 1, justifyContent: 'center' }}>
-            <TextInput
-              value={phoneNumber}
-              onChangeText={handlePhoneNumberChange}
-              placeholder="(71) 90000-0000"
-              keyboardType="phone-pad"
-              className="flex-1"
-              placeholderTextColor={colors.gray[400]}
-              style={{ fontSize: 16 }} // TODO: Remove default lineHeight from tailwind so that `text-base` class can be used
-            />
+        <View>
+          <View
+            className={cn(
+              'mt-6 h-12 flex-row justify-center rounded border border-gray-200 px-3',
+              showError && 'border-red-500',
+            )}
+          >
+            <Text className="mr-2 self-center text-base text-gray-400" style={{ lineHeight: 20 }}>
+              +55
+            </Text>
+            <View style={{ flex: 1 }}>
+              <TextInput
+                value={phoneNumber}
+                onChangeText={handlePhoneNumberChange}
+                placeholder="(71) 90000-0000"
+                keyboardType="phone-pad"
+                className="flex-1"
+                placeholderTextColor={colors.gray[400]}
+                style={{ fontSize: 16 }} // TODO: Remove default lineHeight from tailwind so that `text-base` class can be used
+              />
+            </View>
           </View>
+          {showError && (
+            <Text className="mt-1 text-xs font-normal text-red-500">
+              Número invalido, verifique.
+            </Text>
+          )}
         </View>
         <TouchableOpacity
           className={cn(
-            'h-12 items-center justify-center rounded bg-blue-700',
+            'mt-6 h-12 items-center justify-center rounded bg-blue-700',
             !isPhoneNumberValid && 'bg-blue-700/[.70]',
           )}
-          disabled={!isPhoneNumberValid}
+          onPress={handleReceiveOTP}
+          activeOpacity={!isPhoneNumberValid ? 1 : 0.2}
         >
-          <Link href="/otpConfirmation" className="text-base font-semibold text-white">
-            Receber SMS
-          </Link>
+          <Text className="text-base font-semibold text-white">Receber SMS</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
