e3e1adfacd90db777bd72c7a211f2e2c5d61948c
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2023-08-24 01:39:24 -0300

    Added OTP input and updated screen

--- app/otpConfirmation.tsx
+++ app/otpConfirmation.tsx
@@ -1,14 +1,65 @@
-import { Feather } from '@expo/vector-icons'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import MessagesIllustration from '../assets/vectors/illustration-messages.svg'
+import OTPInput from '../components/OTPInput'
+import { cn } from '../utils/cn'
+import { useEffect, useState } from 'react'
+import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
 
 export default function OTPConfirmation() {
+  const [otpCode, setOTPCode] = useState('')
+  const [isPinReady, setIsPinReady] = useState(false)
+  const maximumCodeLength = 4
+  const [showError, setShowError] = useState(false)
+
+  const handleConfirmOTP = () => {
+    if (!isPinReady) return setShowError(true)
+  }
+
+  useEffect(() => {
+    setShowError(false)
+  }, [isPinReady])
+
   return (
-    <View>
-      <Feather name="arrow-left" size={24} />
-      <Text>Qual o número do seu celular?</Text>
-      <Text>Você receberá um código por SMS</Text>
-      <Link href="/">Go to Sign In</Link>
-    </View>
+    <SafeAreaView className="flex flex-1 bg-white">
+      <View className="flex-1 px-6 py-2">
+        <View className="my-8 w-fit items-center">
+          <MessagesIllustration height={112} pointerEvents="none" />
+        </View>
+        <Text className="text-xl font-medium">
+          Digite o código de 4 dígitos que enviamos por SMS
+        </Text>
+        <Text className="text-base font-normal text-gray-500">
+          SMS enviado para +55 (71) 99315-8381
+        </Text>
+        <View className="items-center">
+          <OTPInput
+            code={otpCode}
+            setCode={setOTPCode}
+            maximumLength={maximumCodeLength}
+            setIsPinReady={setIsPinReady}
+            customClassName="mt-8"
+            showError={showError}
+          />
+          {showError && (
+            <Text className="mt-2 text-xs font-normal text-red-500">
+              {`O código deve conter ${maximumCodeLength} dígitos`}
+            </Text>
+          )}
+        </View>
+        <View className="mt-4 flex-row items-center justify-center">
+          <Text className="text-xs font-normal text-gray-500">Não recebeu?&nbsp;</Text>
+          <Text className="text-xs underline text-blue-900">Reenviar</Text>
+        </View>
+        <TouchableOpacity
+          className={cn(
+            'mt-6 h-12 items-center justify-center rounded bg-blue-700',
+            !isPinReady && 'bg-blue-700/[.70]',
+          )}
+          activeOpacity={!isPinReady ? 1 : 0.2}
+          onPress={handleConfirmOTP}
+        >
+          <Text className="text-base font-semibold text-white">Continuar</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
---
+++ components/OTPInput.tsx
@@ -0,0 +1,101 @@
+import { cn } from '../utils/cn'
+import React, { useRef, useState, useEffect, FC } from 'react'
+import { Pressable, Text, TextInput, View } from 'react-native'
+import { ClassNameValue } from 'tailwind-merge'
+
+const determineAction = (currentCode: string, newCode: string): 'add' | 'delete' | null =>
+  newCode.length > currentCode.length ? 'add' : 'delete'
+
+interface ISingleDigitProps {
+  digit: string
+  isFocused: boolean
+  customClassName?: ClassNameValue
+}
+
+const SingleDigit: FC<ISingleDigitProps> = ({ digit, isFocused, customClassName }) => (
+  <View
+    className={cn(
+      'h-12 justify-center items-center rounded border bg-white border-gray-200 w-12 shadow-md',
+      isFocused && 'border-gray-300',
+      customClassName,
+    )}
+  >
+    <Text className="text-2xl text-center">{digit}</Text>
+  </View>
+)
+
+interface IOTPInputProps {
+  code: string
+  setCode: (code: string) => void
+  maximumLength: number
+  setIsPinReady: (isReady: boolean) => void
+  customClassName?: ClassNameValue
+  showError?: boolean
+}
+
+const OTPInput: FC<IOTPInputProps> = ({
+  code,
+  setCode,
+  maximumLength,
+  setIsPinReady,
+  customClassName,
+  showError,
+}) => {
+  const singleDigitsArray = new Array(maximumLength).fill(null)
+  const inputRef = useRef<TextInput | null>(null)
+  const [isInputBoxFocused, setIsInputBoxFocused] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+
+  const handleOnPress = () => {
+    setIsInputBoxFocused(true)
+    inputRef?.current?.focus()
+  }
+
+  useEffect(() => {
+    setIsPinReady(code.length === maximumLength)
+    return () => setIsPinReady(false)
+  }, [code])
+
+  const handleTextChange = (newCode: string) => {
+    const action = determineAction(code, newCode)
+    if (action === 'add') {
+      setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, maximumLength - 1))
+    } else if (action === 'delete') {
+      setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0))
+    }
+    setCode(newCode)
+  }
+
+  return (
+    <View className={cn('self-center text-base text-gray-400', customClassName)}>
+      <Pressable onPress={handleOnPress} className="flex-row justify-between">
+        {singleDigitsArray.map((_, index) => {
+          const digit = code[index] || ''
+          const isValueFocused = isInputBoxFocused && index === currentIndex
+
+          return (
+            <SingleDigit
+              key={index}
+              digit={digit}
+              isFocused={isValueFocused}
+              customClassName={cn(
+                index < maximumLength - 1 && 'mr-3',
+                showError && 'border-red-500',
+              )}
+            />
+          )
+        })}
+      </Pressable>
+      <TextInput
+        value={code}
+        onChangeText={handleTextChange}
+        maxLength={maximumLength}
+        ref={inputRef}
+        onBlur={() => setIsInputBoxFocused(false)}
+        className="absolute opacity-0"
+      />
+    </View>
+  )
+}
+
+export default OTPInput
