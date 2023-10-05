4a87d9cda819c2edf876b96eb4ccdc24c124f44d
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2023-10-05 10:24:16 -0300

    Added ability to block times according to duration

--- core/services/daySchedules.ts
+++ core/services/daySchedules.ts
@@ -2,17 +2,33 @@ import { daySchedulesRepository } from "../repositories/daySchedules"
 import { IDate } from "../../components/appointment"
 import { EDateFormats, dayjs } from '../../utils/dayjs';
 
-const bitStringToTimeArray = (bitString: string, intervalMinutes: number): string[] => {
-  return bitString.split('')
-    .map((bit, index) => (bit === '0' ? dayjs().startOf('day').add(index * intervalMinutes, 'minute').format(EDateFormats.TIME_ID) : null))
-    .filter(time => time !== null) as string[];
+const bitStringToTimeArray = (bitString: string, intervalMinutes: number, duration: number): string[] => {
+  const bits = bitString.split('');
+  const requiredBits = Math.ceil(duration / intervalMinutes);
+
+  const allDates = bits.map((bit, index) => {
+    const availableForDuration = bits.slice(index, index + requiredBits).every(b => b === '0');
+    return availableForDuration
+      ? dayjs().startOf('day').add(index * intervalMinutes, 'minute').format(EDateFormats.TIME_ID)
+      : null;
+  });
+
+  const availableDates = allDates.filter(time => time !== null) as string[];
+  return availableDates;
 }
 
 export const daySchedulesService = {
-  getAvailableTimesByDate: async (dateId: IDate['id']): Promise<string[]> => {
+  getAvailableTimesByDate: async (dateId: IDate['id'], duration: number): Promise<string[]> => {
     const daySchedule = await daySchedulesRepository.getOneDaySchedule(dateId)
+    if (!daySchedule) return bitStringToTimeArray('0'.repeat(48), 30, duration);
 
-    if (!daySchedule) return bitStringToTimeArray('0'.repeat(48), 30);
-    return bitStringToTimeArray(daySchedule.busyTimes, 30);
+    const employeesAvailabilities = Object.values(daySchedule.employees).map(schedule => {
+      return bitStringToTimeArray(schedule, 30, duration)
+    })
+
+    const mergedAvailabilities = ([] as string[]).concat(...employeesAvailabilities);
+    const uniqueAvailabilities = [...new Set(mergedAvailabilities)];
+
+    return uniqueAvailabilities
   }
 }
