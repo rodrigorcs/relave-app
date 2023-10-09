0b8a81431a080ce105fc4a0b5d0e53174aebd7b5
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2023-10-09 18:19:40 -0300

    Fixed prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
