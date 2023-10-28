e827446a2eef691441ef3e00ff09e148ad4c6dc9
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2023-10-28 10:46:10 -0300

    Moved baseUrls to to config

--- config/config.ts
+++ config/config.ts
@@ -1,12 +1,24 @@
 import { EnvVariables } from "../models/constants/EnvVariables";
+import { isAndroid } from "../utils/platform";
 
 const PROJECT_NAME = 'relave'
+const REGION = 'southamerica-east1'
+const FIREBASE_PROJECT_ID = `${PROJECT_NAME}-${EnvVariables.STAGE}` as const
+
+const LOCALHOST_ADDRESS = isAndroid ? '10.0.2.2' : 'localhost' as const
+const FIREBASE_REMOTE_FUNCTIONS_URL = `https://${REGION}-${FIREBASE_PROJECT_ID}.cloudfunctions.net` as const
+const FIREBASE_LOCAL_FUNCTIONS_URL = `http://${LOCALHOST_ADDRESS}:5001/${FIREBASE_PROJECT_ID}/${REGION}` as const
+
+const IS_PROD = EnvVariables.STAGE === 'prod'
+const IS_LOCAL = EnvVariables.STAGE === 'local'
 
 export const Config = {
-  REGION: 'southamerica-east1',
+  REGION,
   PROJECT_NAME,
   STAGE: EnvVariables.STAGE,
-  FIREBASE_PROJECT_ID: `${PROJECT_NAME}-${EnvVariables.STAGE}`,
-  IS_PROD: EnvVariables.STAGE === 'prod',
-  IS_LOCAL: EnvVariables.STAGE === 'local'
+  FIREBASE_PROJECT_ID,
+  IS_PROD,
+  IS_LOCAL,
+  GOOGLE_MAPS_API_URL: 'https://maps.googleapis.com/maps/api',
+  FIREBASE_CLOUD_FUNCTIONS_URL: IS_LOCAL ? FIREBASE_LOCAL_FUNCTIONS_URL : FIREBASE_REMOTE_FUNCTIONS_URL
 } as const
--- models/constants/Endpoints.ts
+++ models/constants/Endpoints.ts
@@ -1,16 +1,10 @@
-import { isAndroid } from '../../utils/platform'
 import { Config } from '../../config/config'
 import { IOnboardingHint } from '../contracts/onboardingHint'
 import { IVehicleBrand } from '../contracts/vehicleBrand'
 
-const LOCALHOST_ADDRESS = isAndroid ? '10.0.2.2' : 'localhost'
-const LOCAL_CLOUD_FUNCTIONS_URL = `http://${LOCALHOST_ADDRESS}:5001/${Config.FIREBASE_PROJECT_ID}/${Config.REGION}`
-
-const FIREBASE_CLOUD_FUNCTIONS_URL = `https://${Config.REGION}-${Config.FIREBASE_PROJECT_ID}.cloudfunctions.net`
-
 const BaseUrls = {
-  GOOGLE_MAPS_API: 'https://maps.googleapis.com/maps/api',
-  FIREBASE_CLOUD_FUNCTIONS: Config.IS_LOCAL ? LOCAL_CLOUD_FUNCTIONS_URL : FIREBASE_CLOUD_FUNCTIONS_URL,
+  GOOGLE_MAPS_API: Config.GOOGLE_MAPS_API_URL,
+  FIREBASE_CLOUD_FUNCTIONS: Config.FIREBASE_CLOUD_FUNCTIONS_URL,
 }
 
 export const Endpoints = {
